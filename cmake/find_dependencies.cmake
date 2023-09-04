5be46e19d475f473aaa9e640f040680ddde060ac
bb <bumblebeeauv@gmail.com>
2023-09-04 18:10:59 +0800

    Building OpenVDB from source doesn't work, disable

--- cmake/find_dependencies.cmake
+++ cmake/find_dependencies.cmake
@@ -25,11 +25,13 @@ if(USE_SYSTEM_OPENVDB)
   # of it
   include(GNUInstallDirs)
   list(APPEND CMAKE_MODULE_PATH "${CMAKE_INSTALL_FULL_LIBDIR}/cmake/OpenVDB")
-  find_package(OpenVDB QUIET)
+  find_package(OpenVDB)
   if(OpenVDB_FOUND AND OpenVDB_USES_BLOSC)
     # We need to get these hidden dependencies (if available) to static link
     # them inside our library
     target_link_libraries(OpenVDB::openvdb INTERFACE Blosc::blosc)
+  else()
+    message(WARNING "${CMAKE_MODULE_PATH} ${OpenVDB_FOUND} not found")
   endif()
 endif()
 
@@ -39,5 +41,5 @@ endif()
 if(NOT USE_SYSTEM_OPENVDB OR NOT OpenVDB_FOUND)
   set(USE_SYSTEM_OPENVDB OFF)
   message(WARNING "Building OpenVDB from source")
-  include(${CMAKE_CURRENT_LIST_DIR}/OpenVDB/OpenVDB.cmake)
+  #include(${CMAKE_CURRENT_LIST_DIR}/OpenVDB/OpenVDB.cmake)
 endif()
